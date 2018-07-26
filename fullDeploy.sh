ba93ae0cb37838f66cf01ec99bd156c9dc313d81
mrolland <mrolland@debian.bnl.gov>
2018-07-26 11:17:43 -0400

    Copy even less stuff

--- fullDeploy.sh
+++ fullDeploy.sh
@@ -64,13 +64,13 @@ if ! [ -z $TARGET ]; then
 	echo copying ADCore...
 	mkdir -p $AD_DIR/ADCore/ADApp
 	cp -r -n $DETECTOR/ADCore/bin $AD_DIR/ADCore
-	cp -r -n $DETECTOR/ADCore/lib $AD_DIR/ADCore
+	# cp -r -n $DETECTOR/ADCore/lib $AD_DIR/ADCore
 	cp -r -n $DETECTOR/ADCore/db $AD_DIR/ADCore
-	cp -r -n $DETECTOR/ADCore/documentation $AD_DIR/ADCore
+	# cp -r -n $DETECTOR/ADCore/documentation $AD_DIR/ADCore
 	cp -r -n $DETECTOR/ADCore/iocBoot $AD_DIR/ADCore
-	cp -r -n $DETECTOR/ADCore/Viewers $AD_DIR/ADCore
+	# cp -r -n $DETECTOR/ADCore/Viewers $AD_DIR/ADCore
 	cp -r -n $DETECTOR/ADCore/ADApp/Db $AD_DIR/ADCore/ADApp
-	cp -r -n $DETECTOR/ADCore/ADApp/op $AD_DIR/ADCore/ADApp
+	# cp -r -n $DETECTOR/ADCore/ADApp/op $AD_DIR/ADCore/ADApp
 
 	echo looking for ADSupport...
 	ADSUPPORT="$(ls $DETECTOR | grep -m 1 ADSupport)"
@@ -78,8 +78,8 @@ if ! [ -z $TARGET ]; then
 	    echo copying $ADSUPPORT...
 	    mkdir -p $AD_DIR/$ADSUPPORT
 	    cp -r -n $DETECTOR/$ADSUPPORT/bin $AD_DIR/$ADSUPPORT
-	    cp -r -n $DETECTOR/$ADSUPPORT/lib $AD_DIR/$ADSUPPORT
-	    cp -r -n $DETECTOR/$ADSUPPORT/supportApp $AD_DIR/$ADSUPPORT
+	    # cp -r -n $DETECTOR/$ADSUPPORT/lib $AD_DIR/$ADSUPPORT
+	    # cp -r -n $DETECTOR/$ADSUPPORT/supportApp $AD_DIR/$ADSUPPORT
 	    echo done.
 	else
 	    echo Not found.
@@ -92,8 +92,8 @@ if ! [ -z $TARGET ]; then
 	    mkdir -p $TARGET/$ASYN
 	    cp -r -n $SUPPORT/$ASYN/bin $TARGET/$ASYN
 	    cp -r -n $SUPPORT/$ASYN/db $TARGET/$ASYN
-	    cp -r -n $SUPPORT/$ASYN/opi $TARGET/$ASYN
-	    cp -r -n $SUPPORT/$ASYN/lib $TARGET/$ASYN
+	    # cp -r -n $SUPPORT/$ASYN/opi $TARGET/$ASYN
+	    # cp -r -n $SUPPORT/$ASYN/lib $TARGET/$ASYN
 	    echo done.
 	else
 	    echo Not found.
@@ -105,7 +105,7 @@ if ! [ -z $TARGET ]; then
 	    echo copying $SAVE...
 	    mkdir -p $TARGET/$SAVE/asApp
 	    cp -r -n $SUPPORT/$SAVE/asApp/Db $TARGET/$SAVE/asApp
-	    cp -r -n $SUPPORT/$SAVE/asApp/op $TARGET/$SAVE/asApp
+	    # cp -r -n $SUPPORT/$SAVE/asApp/op $TARGET/$SAVE/asApp
 	    cp -r -n $SUPPORT/$SAVE/bin $TARGET/$SAVE
 	    echo done.
 	else
@@ -118,7 +118,7 @@ if ! [ -z $TARGET ]; then
 	    echo copying $BUSY...
 	    mkdir -p $TARGET/$BUSY/busyApp
 	    cp -r -n $SUPPORT/$BUSY/busyApp/Db $TARGET/$BUSY/busyApp
-	    cp -r -n $SUPPORT/$BUSY/busyApp/op $TARGET/$BUSY/busyApp
+	    # cp -r -n $SUPPORT/$BUSY/busyApp/op $TARGET/$BUSY/busyApp
 	    echo done.
 	fi
 
@@ -128,7 +128,7 @@ if ! [ -z $TARGET ]; then
 	    echo copying $CALC...
 	    mkdir -p $TARGET/$CALC/calcApp
 	    cp -r -n $SUPPORT/$CALC/calcApp/Db $TARGET/$CALC/calcApp
-	    cp -r -n $SUPPORT/$CALC/calcApp/op $TARGET/$CALC/calcApp
+	    # cp -r -n $SUPPORT/$CALC/calcApp/op $TARGET/$CALC/calcApp
 	    echo done.
 	else
 	    echo Not found.
@@ -140,9 +140,9 @@ if ! [ -z $TARGET ]; then
 	    echo copying $DSTATS...
 	    mkdir -p $TARGET/$DSTATS
 	    cp -r -n $SUPPORT/$DSTATS/bin $TARGET/$DSTATS
-	    cp -r -n $SUPPORT/$DSTATS/lib $TARGET/$DSTATS
+	    # cp -r -n $SUPPORT/$DSTATS/lib $TARGET/$DSTATS
 	    cp -r -n $SUPPORT/$DSTATS/db $TARGET/$DSTATS
-	    cp -r -n $SUPPORT/$DSTATS/op $TARGET/$DSTATS
+	    # cp -r -n $SUPPORT/$DSTATS/op $TARGET/$DSTATS
 	    echo done.
 	else
 	    echo Not found.
@@ -154,9 +154,9 @@ if ! [ -z $TARGET ]; then
 	    echo copying $STATS...
 	    mkdir -p $TARGET/$STATS
 	    cp -r -n $SUPPORT/$STATS/bin $TARGET/$STATS
-	    cp -r -n $SUPPORT/$STATS/lib $TARGET/$STATS
+	    # cp -r -n $SUPPORT/$STATS/lib $TARGET/$STATS
 	    cp -r -n $SUPPORT/$STATS/db $TARGET/$STATS
-	    cp -r -n $SUPPORT/$STATS/op $TARGET/$STATS
+	    # cp -r -n $SUPPORT/$STATS/op $TARGET/$STATS
 	    echo done.
 	else
 	    echo Not found.
@@ -168,7 +168,7 @@ if ! [ -z $TARGET ]; then
 	    echo copying $SCAN...
 	    mkdir -p $TARGET/$SCAN/sscanApp
 	    cp -r -n $SUPPORT/$SCAN/sscanApp/Db $TARGET/$SCAN/sscanApp
-	    cp -r -n $SUPPORT/$SCAN/sscanApp/op $TARGET/$SCAN/sscanApp
+	    # cp -r -n $SUPPORT/$SCAN/sscanApp/op $TARGET/$SCAN/sscanApp
 	    echo done.
 	else
 	    echo Not found.
@@ -179,7 +179,7 @@ if ! [ -z $TARGET ]; then
 	if ! [ -z $SEQ ]; then
 	    echo copying $SEQ...
 	    mkdir -p $TARGET/$SEQ
-	    cp -r -n $SUPPORT/$SEQ/lib $TARGET/$SEQ
+	    # cp -r -n $SUPPORT/$SEQ/lib $TARGET/$SEQ
 	    cp -r -n $SUPPORT/$SEQ/bin $TARGET/$SEQ
 	    echo done.
 	else
@@ -226,7 +226,6 @@ if ! [ -z $TARGET ]; then
 	cd $HOME
 	cd $TARGET
 	echo \# put this file in IOC folders to use this deployment. > envPaths
-	echo \# plugins added with [absolute]fullDeploy.sh will automatically have this file with appropriate paths copied in. >> envPaths
 	echo >> envPaths
 	echo epicsEnvSet\(\"SUPPORT\", \"$SUPPORT_DIR\"\) >> envPaths
 	echo epicsEnvSet\(\"ASYN\", \"$ASYN_DIR\"\) >> envPaths
@@ -252,14 +251,12 @@ if ! [ -z $TARGET ]; then
 	echo "This deployment of areaDetector was created by absoluteFullDeploy.sh" > README.txt
 	echo "from https://github.com/rollandmichae7/ioc_deploy" >> README.txt
 	echo >> README.txt
-	echo "To use IOCs with this deployment, copy the envPaths file" >> README.txt
+	echo "To use this deployment wit an IOC, copy the envPaths file" >> README.txt
 	echo "from this directory into the IOC's directory and fill in" >> README.txt 
 	echo "the 3 required plugin-dependent variables in the file." >> README.txt
 	echo "For plugins that use st.cmd.linux and st.cmd.windows," >> README.txt
 	echo "copy envPaths as envPaths.linux or envPaths.windows" >> README.txt
 	echo "according to to your architecture." >> README.txt
-	echo "Any plugin added during the running of the script" >> README.txt
-	echo "will already have envPaths configured." >> README.txt
 	echo >> README.txt
 	echo "WARNING: envPaths uses absolute paths based on the location" >> README.txt
 	echo "of this deployment's initial creation. These will break if this" >> README.txt
@@ -298,6 +295,29 @@ if ! [ -z $TARGET ]; then
 		# cp -r -n $DETECTOR/$PLUGIN/lib $AD_DIR/$PLUGIN
 		# cp -r -n $DETECTOR/$PLUGIN/$APP/Db $AD_DIR/$PLUGIN/$APP
 		# cp -r -n $DETECTOR/$PLUGIN/$APP/op $AD_DIR/$PLUGIN/$APP
+		echo done.
+
+		echo making envPaths file...		
+		cd $AD_DIR/$PLUGIN
+		PLUG_ABSPATH=$(pwd)
+		cd $HOME
+		IOC_DIR="$(ls -d1 $PLUG_ABSPATH/iocs/** | grep -m 1 IOC)" # iocs/prosilicaIOC
+		IOC_DIR="$(ls -d1 $IOC_DIR/iocBoot/** | grep -m 1 ioc)" # iocBoot/iocProsilica
+		cd $TARGET
+		cp envPaths $IOC_DIR
+		cd $IOC_DIR
+		TOP="../.."
+		PLUGIN_UPPER="$(echo $PLUGIN | tr a-z A-Z)"
+		echo epicsEnvSet\(\"IOC\", \"ioc$PLUGIN\"\) >> envPaths
+		echo epicsEnvSet\(\"TOP\", \"$TOP\"\) >> envPaths
+		echo epicsEnvSet\(\"$PLUGIN_UPPER\", \"$TOP/../..\"\) >> envPaths
+		cp envPaths envPaths.linux
+		cp envPaths envPaths.windows
+		cp st.cmd OLD_st.cmd
+		ENV="$(grep -m 1 '< envPaths' st.cmd)"
+		if [ -z "$ENV" ]; then
+		    sed -i '1i< envPaths' st.cmd
+		fi
 		echo done.
 	    fi
 	    if [ "$CHOICE" = "$2" ]; then
